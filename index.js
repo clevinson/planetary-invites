0af6eec0137ce67e399566f1bc668d76f571fc09
Cory Levinson <cjlevinson@gmail.com>
2019-09-02 16:24:47 +0000

    make more error resilient

--- index.js
+++ index.js
@@ -3,29 +3,39 @@ const express = require('express')
 const port = 8080
 
 let app = express()
-let sbot
 
-app.set('views', './views')
+let ssb;
 
-
-ssbClient(function(err, client) {
-  sbot = client
-})
+ssbClient(function(err, sbot) {
+  ssb = sbot;
+});
 
 app.use(express.static('public'))
-app.get('/invite', function (req, response) {
 
-  let uses = req.query.uses || 1
+app.get('/invite', function (req, response, next) {
+  try {
+    let uses = req.query.uses || 1
 
-  sbot.invite.create({'uses': uses}, function(err, inviteCode) {
-    if (err) {
-      console.log(err)
-      response.send(err)
-    } else {
-      console.log(`[${new Date().toISOString()}]: Generated ${uses} invites`)
-      response.send({'inviteCode': inviteCode, 'uses': uses})
-    }
-  })
+    ssb.invite.create({'uses': uses}, function(err, inviteCode) {
+      if (err) {
+        next(err)
+      } else {
+        console.log(`[${new Date().toISOString()}]: Generated ${uses} invites`)
+        response.send({'inviteCode': inviteCode, 'uses': uses})
+      }
+    })
+  } catch (err) {
+
+    ssbClient(function(err, sbot) {
+      ssb = sbot;
+    })
+    
+    next(err)
+  }
 })
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send({'error': "Invite generation failed!"}) })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
